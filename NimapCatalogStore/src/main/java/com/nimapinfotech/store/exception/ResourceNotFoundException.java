f2b4b855c3bd736e07b281573940ef31796744a2
Vishal0001V <vishalkailasbhume@gmail.com>
2025-01-22 12:59:33 +0530

    Update ResourceNotFoundException.java
    Done

--- NimapCatalogStore/src/main/java/com/nimapinfotech/store/exception/ResourceNotFoundException.java
+++ NimapCatalogStore/src/main/java/com/nimapinfotech/store/exception/ResourceNotFoundException.java
@@ -4,4 +4,4 @@ public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
     }
-}
+}//this class is the genrated to Use our own exceptions
